6e7316068c1e5403d2d84f2b1bb2f5d3ae3efbb1
ItzAEzClap <karlsson_2@outlook.com>
2023-09-19 11:16:55 +0200

    bättre onlinekod

--- online.js
+++ online.js
@@ -6,7 +6,11 @@ while (emptyString.length < 6) {
 }
 const peerId = emptyString;
 
-
+/*
+Object.keys(connections).forEach(id => {
+    connections[id].send('TESTING')
+})
+*/
 
 const peer = new Peer(peerId, { debug: 1 })
 var connections = {};
@@ -18,22 +22,46 @@ peer.on('connection', x => {
     x.on('open', () => {
         if (host) {
             connections[x.peer] = peer.connect(x.peer);
+            connections[x.peer].on("open", () => {
+                console.log("Connected to: " + x.peer)
+            })
+            connections[x.peer].on("close", () => {
+                console.log(x.peer + " disconnected")
+                delete connections[x.peer]
+            })
         }
     });
-    x.on('data', data => {
-        console.log(data)
-        if (host) {
 
-        } else {
-
+    x.on('data', data => {    
+        if (data.type === 'QUESTION') {
+            // Show Question
+        } else if (data.type === 'ANSWER') {
+            // Send Clicked Answer
         }
     })
+
 });
+
+
+function sendData(data) {
+    if (!host) { connection.send(data); return }
+
+    for (let con of Object.values(connections)) {
+        con.send(data)
+    }
+}
 
 function connect() {
     const connectTo = document.getElementById('inputId').value
     if (connectTo != peerId) {
         connection = peer.connect(connectTo);
+        connection.on("open", () => {
+            console.log("connected to host")
+        })
+        connection.on("close", () => {
+            console.log("host disconnected")
+            connection = undefined
+        })
     }
 }
 
@@ -43,6 +71,4 @@ function initGame() {
     document.getElementById('connectbutton').style.visibility = "hidden"
     document.getElementById('hostbutton').style.visibility = "hidden"
     document.getElementById("thisId").innerHTML = "Your Id: " + peerId;
-
 }
-
