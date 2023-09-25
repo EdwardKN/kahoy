d8738d84a4cb188237b674d3d92b1766af851503
ItzAEzClap <karlsson_2@outlook.com>
2023-09-25 17:16:18 +0200

    idk

--- sharedfunctions.js
+++ sharedfunctions.js
@@ -1,3 +1,68 @@
+function sendData(item, callback) {
+    const http = new XMLHttpRequest();
+    let url = "https://l2niipto9l.execute-api.eu-north-1.amazonaws.com/EdwardKN/updatekahoyusers?";
+    Object.entries(item).forEach(e => {
+        url += e[0] + "=" + e[1] + "&";
+    })
+
+    http.open("GET", url);
+    http.send();
+
+
+    http.onreadystatechange = (e) => {
+        if (http.readyState === 4) {
+            callback(http.responseText)
+        }
+    };
+};
+function getUser(callback) {
+    const http = new XMLHttpRequest();
+    const url = `https://l2niipto9l.execute-api.eu-north-1.amazonaws.com/EdwardKN/getkahoyusers?session=${localStorage.getItem('session')}`;
+    http.open("GET", url);
+    http.send();
+
+    http.onreadystatechange = (e) => {
+        if (http.readyState === 4) {
+            callback(JSON.parse(http.responseText));
+        }
+    };
+};
+
+function getPublicGames(callback){
+    const http = new XMLHttpRequest();
+    const url = `https://l2niipto9l.execute-api.eu-north-1.amazonaws.com/EdwardKN/getkahoyusers?getpublic=true`;
+    http.open("GET", url);
+    http.send();
+
+    http.onreadystatechange = (e) => {
+        if (http.readyState === 4) {
+            callback(JSON.parse(http.responseText));
+        }
+    };
+}
+
+function checkPassword(username, password, callback) {
+    const http = new XMLHttpRequest();
+    const url = `https://l2niipto9l.execute-api.eu-north-1.amazonaws.com/EdwardKN/getkahoyusers?username=${username}&password=${password}`;
+    http.open("GET", url);
+    http.send();
+
+    http.onreadystatechange = (e) => {
+        if (http.readyState === 4) {
+            callback(JSON.parse(http.responseText));
+        }
+    };
+};
+
+function shuffle(unshuffled){
+    let shuffled = unshuffled
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+
+    return shuffled
+}
+
 const questionTypes = ["Single answer", "Multiple choice"]
 const colors = ['red', 'blue', 'orange', 'green']
 
@@ -9,16 +74,6 @@ function generateId(length) {
     }
     
     return id
-}
-
-function shuffle(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1))
-        let temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
-    }
-    return arr
 }
 
 async function fetchHTML(document, url, prefix) {
