d869ff118364826700427c2ff18d61c6fece59f7
Edward <edward.klinghammer@gmail.com>
2023-09-26 19:43:16 +0200

    visar username i menyn

--- main/checklogin.js
+++ main/checklogin.js
@@ -1,14 +1,26 @@
 if (localStorage.getItem("session")) {
-    document.getElementById("login").textContent = "Logout";
-    document.getElementById("signUp").remove();
-    document.getElementById("login").onclick = function () {
+    document.getElementById("signUp").textContent = "Logout";
+    document.getElementById("login").textContent = "Logged in as";
+    document.getElementById("login").onclick = undefined;
+    document.getElementById("signUp").onclick = function () {
         localStorage.clear();
         window.location.reload();
     }
+    getUser(function (user) {
+        if (localStorage.getItem("session")) {
+            document.getElementById("login").textContent = user.username;
+        } else {
+            document.getElementById("login").textContent = "Login";
+        }
+    })
 } else {
     document.getElementById("login").onclick = function () {
         window.location.replace('./../login/index.html')
     }
+    document.getElementById("signUp").onclick = function () {
+        window.location.replace('./../login/create.html');
+    }
     document.getElementById("create").remove();
 
 }
+
